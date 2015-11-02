da124d41ee34c9d4483fa4da05bb6651f2a21cc9
unknown <stouvoli@gmail.com>
2015-11-02 00:19:20 +0000

    Developing an MVC Component/Adding a view to the site part

--- com_helloworld/site/views/helloworld/tmpl/default.php
+++ com_helloworld/site/views/helloworld/tmpl/default.php
@@ -6,13 +6,8 @@
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+ 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
-/**
- * Hello World Component Controller
- *
- * @since  0.0.1
- */
-class HelloWorldController extends JControllerLegacy
-{
-}
+?>
+<h1><?php echo $this->msg; ?></h1>
